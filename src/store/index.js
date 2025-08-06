03ede3a0fcbc25ed36cc8f853098f80eeb7e4f81
Admin <gs.techdeveloper@gmail.com>
2025-08-05 23:39:39 -0300

    Projeto criado

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    registros: []
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container p-5">
+    <form-component></form-component>
+    <list-component></list-component>
+  </div>
+</template>
+
+<style>
+  body{
+    background: #111 !important;
+  }
+</style>
+
+<style scoped>
+
+</style>
+
+<script>
+import FormComponent from '@/components/FormComponent.vue';
+import ListComponent from '@/components/ListComponent.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    FormComponent,
+    ListComponent
+  }
+}
+
+</script>
